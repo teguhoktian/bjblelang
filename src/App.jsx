902ee3f0c6beb853323f60209a6fc3ebbd221d62
Hoki Teguh Oktian <oktian89@gmail.com>
2024-05-22 16:05:00 +0700

    Add Font Robotto to Vite Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="font-Roboto">
+        <h1 className="text-3xl font-bold underline">Hello,</h1>
+        <p>Ini akan menjadi aplikasi yang sangat bagus</p>
+      </div>
     </>
   );
 }
