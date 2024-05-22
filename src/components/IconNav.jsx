7039dbc4dbcb1eb9a35720035b84aeb7301f019a
Hoki Teguh Oktian <oktian89@gmail.com>
2024-05-22 21:53:27 +0700

    Icon Navigation Component

---
+++ src/components/IconNav.jsx
@@ -0,0 +1,37 @@
+import {
+  Cottage,
+  Grass,
+  Handshake,
+  LocalFireDepartment,
+  MonetizationOn,
+} from "@mui/icons-material";
+import React from "react";
+
+function IconNav() {
+  return (
+    <div className="mx-auto pt-8 font-Roboto grid grid-cols-6 w-3/5">
+      <a className="items-center flex flex-col">
+        <Cottage fontSize="large" className="text-blue-800" />
+        <span className="text-xs">Rumah</span>
+      </a>
+      <a className="items-center flex flex-col">
+        <Grass fontSize="large" className=" text-amber-700" />
+        <span className="text-xs">Tanah</span>
+      </a>
+      <a className="items-center flex flex-col">
+        <LocalFireDepartment fontSize="large" className="text-red-600" />
+        <span className="text-xs">Hot Item</span>
+      </a>
+      <a className="items-center flex flex-col">
+        <Handshake className="text-gray-400" fontSize="large" />
+        <span className="text-xs">Sukarela</span>
+      </a>
+      <a className="items-center flex flex-col">
+        <MonetizationOn fontSize="large" className="text-orange-600" />
+        <span className="text-xs">Lelang</span>
+      </a>
+    </div>
+  );
+}
+
+export default IconNav;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import IconNav from "../components/IconNav";
 
 function Home() {
   return (
     <div>
+      {/* Top Navigation */}
       <Navbar />
+
+      {/* Icon Navbar */}
+      <IconNav />
     </div>
   );
 }
